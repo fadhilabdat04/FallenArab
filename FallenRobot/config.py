62139b28fb975836f0c4c56d4ec8009384a635dd
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-08-14 04:39:52 +0700

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -2,35 +2,35 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    API_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    API_ID = 17896688
+    API_HASH = "947327cf5ff0053a66bf7951f9db5658"
 
-    CASH_API_KEY = ""  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
+    CASH_API_KEY = "AL76C2F5UYMOVZKP"  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
 
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgres://orneerse:O7lW0ogCEw5_MrZAvgcmHBJ7fBeS2m0r@mahmud.db.elephantsql.com/orneerse"  # A sql database url from elephantsql.com
 
-    EVENT_LOGS = ()  # Event logs channel to note down important bot level events
+    EVENT_LOGS = (-1001795374467)  # Event logs channel to note down important bot level events
 
-    MONGO_DB_URI = ""  # Get ths value from cloud.mongodb.com
+    MONGO_DB_URI = "mongodb+srv://doadmin:7P4N61cvJnu9532z@mongodb2-7a545cbe.mongo.ondigitalocean.com/admin?tls=true&authSource=admin"  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
-    START_IMG = "https://te.legra.ph/file/40eb1ed850cdea274693e.jpg"
+    START_IMG = "https://te.legra.ph/file/d2f257710e964cd8aa0db.jpg"
 
-    SUPPORT_CHAT = "DevilsHeavenMF"  # Your Telegram support group chat username where your users will go and bother you
+    SUPPORT_CHAT = "SiArabGroup"  # Your Telegram support group chat username where your users will go and bother you
 
-    TOKEN = ""  # Get bot token from @BotFather on Telegram
+    TOKEN = "6515197149:AAEicDMoVIsS5KfEPpIorYEsLyuuxX55xkk"  # Get bot token from @BotFather on Telegram
 
-    TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
+    TIME_API_KEY = "6I666ZYMGRHX"  # Get this value from https://timezonedb.com/api
 
-    OWNER_ID = 1356469075  # User id of your telegram account (Must be integer)
+    OWNER_ID = 1948147616  # User id of your telegram account (Must be integer)
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = []  # User id of sudo users
-    DEV_USERS = []  # User id of dev users
-    DEMONS = []  # User id of support users
-    TIGERS = []  # User id of tiger users
-    WOLVES = []  # User id of whitelist users
+    DRAGONS = [1948147616]  # User id of sudo users
+    DEV_USERS = [1948147616]  # User id of dev users
+    DEMONS = [1948147616]  # User id of support users
+    TIGERS = [1948147616]  # User id of tiger users
+    WOLVES = [1948147616]  # User id of whitelist users
 
     ALLOW_CHATS = True
     ALLOW_EXCL = True
